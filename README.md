eb5a4b32b377d404a3cf2e4a935a51bff84522f2
RRoxxxsii <mishabur38@gmail.com>
2023-11-24 14:42:00 +0300

    Настройка инфраструктуры проекта: конфигурация Celery, Redis, MySQL, Django

---
+++ README.md
@@ -0,0 +1,8 @@
+<a href='https://likesoft.notion.site/Python-c476609af7fe48e586cd7bafd639ea1e'>'<h1>Тестовое задание - книжный сервис</h1></a>
+<hr>
+<h4></h4>
+<h2>Технологии: </h2>
+<hr>
+<h3>Запуск: </h3>
+
+
---
+++ celery_app.py
@@ -0,0 +1,12 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery('core', broker_connection_retry_on_startup=True)
+app.config_from_object('django.conf:settings')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
