91c01d56795c221404db2d53143d43b4af9cddcf
RRoxxxsii <mishabur38@gmail.com>
2023-11-24 15:30:32 +0300

    Создал модель Books и провел миграции, зарегистрировал приложение в INSTALLED_APPS

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+
+    title = models.CharField('Название', unique=True, max_length=255)
+    author = models.CharField('Автор', max_length=255)
+    year_published = models.PositiveIntegerField('Год издания')
+    isbn = models.CharField('ISBN', max_length=13)
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+    def __str__(self):
+        return f'{self.author} - {self.title}'
